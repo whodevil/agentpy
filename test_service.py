e1e7987fcae404f949efc7f811ce922aaea5177d
whodevil <whodevil@offthecob.info>
2023-10-02 09:32:31 -0700

    update readme

--- test_service.py
+++ test_service.py
@@ -4,7 +4,7 @@ print(
     requests.post(
         "http://0.0.0.0:10000",
         json={
-            "key": "FAKE_KEY",
+            "key": "A_SECRET_KEY_MADE_UP_FOR_THIS_PROJECT",
             "query": "Best practices for building AI Agents?"
         }
     ).json()
