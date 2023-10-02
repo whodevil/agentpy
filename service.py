726e389b138c67d67366e2ffd6f753c52c11628e
whodevil <whodevil@offthecob.info>
2023-10-01 18:34:52 -0700

    Add api key

--- service.py
+++ service.py
@@ -1,15 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
 
 from agent.agent_setup import agent
+from agent import agent_key
 
 api = FastAPI()
 
 
 class Query(BaseModel):
+    key: str
     query: str
 
 
 @api.post("/")
 def research_agent(query: Query):
+    if query.key != agent_key:
+        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={"message": "unauthorized"})
     return agent({"input": query.query})['output']
--- test_service.py
+++ test_service.py
@@ -4,6 +4,7 @@ print(
     requests.post(
         "http://0.0.0.0:10000",
         json={
+            "key": "FAKE_KEY",
             "query": "Best practices for building AI Agents?"
         }
     ).json()
